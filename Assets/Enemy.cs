8f21fbef4c2f4f929d872e2ea6ed90a9b5222e19
emilizz1 <slibinas2012@gmail.com>
2020-07-20 23:03:46 +0300

    Initial commit Started working on enemy

---
+++ Assets/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health, speed;
+
+    bool alive;
+
+    void Start()
+    {
+        StartCoroutine(Move());
+    }
+
+    IEnumerator Move()
+    {
+        int pahtStep = 0;
+        Vector3 currentTarget = PathController.instance.paht[pahtStep];
+        while (alive)
+        {
+            yield return null;
+            Vector2.MoveTowards(transform.position, currentTarget, speed);
+            if(Vector2.Distance(currentTarget, transform.position) < 0.01f)
+            {
+                pahtStep++;
+                currentTarget = PathController.instance.paht[pahtStep];
+            }
+        }
+    }
+
+    public void DealDamage(float _damage)
+    {
+        health -= _damage;
+        if(health <= 0)
+        {
+            EnemyKilled();
+        }
+    }
+
+    void EnemyKilled()
+    {
+        alive = false;
+    }
+}
---
+++ Assets/PathController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathController : MonoBehaviour
+{
+    public static PathController instance;
+
+    public List<Vector3> paht;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
